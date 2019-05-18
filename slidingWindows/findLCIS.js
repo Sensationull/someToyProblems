47ee88af898b65e796bfa84f11620153cd39a672
andrewjohnso7 <andrewjohnso17@gmail.com>
2019-05-18 14:55:32 -0700

    changed for loop to match eslint rules

--- slidingWindows/findLCIS.js
+++ slidingWindows/findLCIS.js
@@ -1,22 +1,20 @@
 const findLengthOfLCIS = (nums) => {
   /*
-    create counter variable set to one
+    create counter variable?
     create max
-    if array length = 0,
-      return 0
     iterate through array
       if element is > than previous,
         inc count
         if count > max
           max = count
-      else
-      reset count 1
+      if element <= previous
+      reset count
     return max > 0 if so, return max else 1?
   */
   if (nums.length === 0) return 0;
   let count = 1;
   let max = 0;
-  for (let i = 1; i < nums.length; i + 1) {
+  for (let i = 1; i < nums.length; i += 1) {
     const cur = nums[i];
     const previous = nums[i - 1];
     if (cur > previous) {
@@ -29,12 +27,12 @@ const findLengthOfLCIS = (nums) => {
   return max > 0 ? max : 1;
 };
 
-
 const input1 = [1, 3, 5, 4, 7]; // 3 // [1,3,5]
 const input2 = [2, 2, 2, 2, 2]; // 1 // [2];
 const test = [1, 3, 5, 7];
 const input3 = [-2, -12, -200, -1300]; // 1 // [-2];
 const input4 = [1, 3, 5, 4, 2, 3, 4, 5]; // 4
+
 console.log( '-->', findLengthOfLCIS(input1)); // 3
 console.log( '-->', findLengthOfLCIS(test)); // 4
 console.log( '-->', findLengthOfLCIS(input2)); // 1
