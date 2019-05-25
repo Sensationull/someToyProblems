af516f27ceaf4ead8a58104a0d5a9bcd6c8bfc7f
andrewjohnso7 <andrewjohnso17@gmail.com>
2019-05-24 18:57:21 -0700

    added postorder solution

---
+++ trees/postorderTraversal.js
@@ -0,0 +1,34 @@
+const example = require('./exampleTree');
+
+const { nTree1 } = example;
+
+const postorderTraversal = (root) => {
+  /*
+  create result arr;
+  create helper
+  iterate through current node's children
+  if no children,
+    push current node
+    then return
+  if children
+    call helper on each child;
+  */
+  const result = [];
+  const helper = (node) => {
+    if (!node) return
+    if (!node.children) {
+      result.push(node.val);
+      return;
+    }
+    if (node.children) {
+      for (let i = 0; i < node.children.length; i += 1) {
+        helper(node.children[i]);
+      }
+      result.push(node.val);
+    }
+  };
+  helper(root);
+  return result;
+};
+
+console.log( '-->', postorderTraversal(nTree1));
