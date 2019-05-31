b52a7c27e1902aca692ddad1b38d7f18d63aad96
andrewjohnso7 <andrewjohnso17@gmail.com>
2019-05-30 18:15:21 -0700

    removed unecessary example

--- trees/averageOfLevels.js
+++ trees/averageOfLevels.js
@@ -1,21 +1,4 @@
 /* eslint-disable */
-
-function TreeNode(val) {
-  this.val = val;
-  this.left = this.right = null;
-}
-
-let root = new TreeNode(3);
-let nine = new TreeNode(9);
-let twenty = new TreeNode(20);
-let fifteen = new TreeNode(15);
-let seven = new TreeNode(7);
-
-root.left = nine;
-root.right = twenty;
-twenty.left = fifteen;
-twenty.right = seven;
-
 /* dfs solution 
   helperFunction -> calculates the average 
   create obj that keeps track of level -> {1: [1], 2: [2, 3], 3: [6, 6, 6, 6]}
